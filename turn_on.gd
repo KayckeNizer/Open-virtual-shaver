591e78af66b20ee7796def68053414feacbc529f
Kaycke <93611525+KayckeNizer@users.noreply.github.com>
2022-07-02 21:53:31 -0300

    Add files via upload

---
+++ turn_on.gd
@@ -0,0 +1,14 @@
+extends Button
+
+func _ready():
+	$background/animation_on.play("turn_off")
+
+func _on_turn_on_button_pressed():
+	$shaver_audio.play()
+	print("Button pressed")
+	$background/animation_on.play("turn_on")
+
+func _on_turn_on_button_button_up():
+	$shaver_audio.stop()
+	print("Button released")
+	$background/animation_on.play("turn_off")
