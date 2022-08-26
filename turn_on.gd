97b12e8e3db87b67d1d1c705287bf0192b57ce15
Kaycke <93611525+GodOfInkBR@users.noreply.github.com>
2022-08-26 16:54:55 -0300

    Vibration added

--- turn_on.gd
+++ turn_on.gd
@@ -1,7 +1,7 @@
 extends TouchScreenButton
 
-var touched := false
-var vibrating := false
+var pressed_button := false
+var vib_duration := 0.5
 
 func _ready():
 	get_parent().get_node("background/animation_on").play("turn_off")
@@ -10,14 +10,23 @@ func _on_TouchScreenButton_pressed():
 	get_parent().get_node("background/shaver_audio").play()
 	print("Button pressed")
 	get_parent().get_node("background/animation_on").play("turn_on")
+	Input.vibrate_handheld(500)
+	pressed_button = true
 
-	touched = true
+
 
 func _on_TouchScreenButton_released():
 	get_parent().get_node("background/shaver_audio").stop()
 	print("Button released")
 	get_parent().get_node("background/animation_on").play("turn_off")
-	touched = false
+	vib_duration = 0.5
+	pressed_button = false
 	
+func _process(delta):
+	vib_duration -= delta
+	
+	if pressed_button and vib_duration <= 0:
+		vib_duration = 0.5
+		Input.vibrate_handheld(500)
 
 		
