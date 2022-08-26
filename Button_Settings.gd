dfea4c07183d7c6390ad07627df6ef0484de0812
Kaycke <93611525+GodOfInkBR@users.noreply.github.com>
2022-08-26 10:33:07 -0300

    Button renamed
    I've just renamed the settings menu button

--- Button_Settings.gd
+++ Button_Settings.gd
@@ -1,10 +1,10 @@
 extends Button
 
 func _ready():
-	get_parent().get_node("Control").visible = false
+	get_parent().get_node("Settings_Menu").visible = false
 
 func _on_Button_Settings_pressed():
-	if get_parent().get_node("Control").visible:
-		get_parent().get_node("Control").visible = false
-	elif not get_parent().get_node("Control").visible:
-		get_parent().get_node("Control").visible = true
+	if get_parent().get_node("Settings_Menu").visible:
+		get_parent().get_node("Settings_Menu").visible = false
+	elif not get_parent().get_node("Settings_Menu").visible:
+		get_parent().get_node("Settings_Menu").visible = true
